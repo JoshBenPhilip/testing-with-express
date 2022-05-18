9198d2c49214fbe758d789d3ba1ecaf4c2398d7c
JoshBenPhilip <joshben4c@gmail.com>
2022-05-18 12:04:02 -0400

    semi-working-code

---
+++ index.js
@@ -0,0 +1,15 @@
+import express, {json} from "express";
+
+const app = express()
+app.use(json())
+
+app.get("/pets", (req,res) => {
+    res.send("All the pets I loved before!")
+})
+
+const PORT = 3030
+app.listen (PORT, ()=> {
+    console.log(`We are listening on port localhost://${PORT}/pets`)
+})
+
+export default app
